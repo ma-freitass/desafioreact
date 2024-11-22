c08c48807f9df89cd5d7f37e40278b501faa3930
Maiala Freitas <maiala_rodrigues@hotmail.com>
2024-11-22 19:28:26 -0300

    Estilização home

---
+++ src/Components/Home/style.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const MainStyle = styled.main`
+display: flex;
+align-items: center;
+height: 60vh;
+
+`
+export const Section =styled.section`
+position: relative;
+`
+export const Copo =styled.img`
+position: absolute;
+
+
+
+`
+  
